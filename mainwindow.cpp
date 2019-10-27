1d12cd025ff7011ea0d070a6e5ebab4e03584d93
Vladimir S <xpinguin@users.noreply.github.com>
2019-10-27 14:58:00 +0300

    project outline 1: sql

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,57 @@
 #include "mainwindow.h"
+#include <QMessageBox>
+//#include <QtSql>
 
 MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent)
 {
   setupUi(this);
+
+  m_db = QSqlDatabase::addDatabase("QPSQL");
+  m_db.setHostName("127.0.0.1");
+  m_db.setDatabaseName("mosoblmap");
+  m_db.setUserName("mosoblmap");
+  m_db.setPassword("oblmos");
+}
+
+void MainWindow::on_btnExecSQL_clicked()
+{
+  if (!m_db.open()) {
+    auto err = m_db.lastError();
+    QMessageBox::critical(this, "ОШИБКА ПОДКЛЮЧЕНИЯ", err.text());
+  }
+  ////
+  QMessageBox::information(this, "Текст запроса", txtSQL->toPlainText());
+  ////
+  auto res = m_db.exec(txtSQL->toPlainText());
+  {
+    auto err = res.lastError();
+    if (err.type() != QSqlError::NoError) {
+      QMessageBox::critical(this, "ОШИБКА ВЫПОЛНЕНИЯ ЗАПРОСА", err.text());
+      return;
+    }
+  }
+  ////
+  if (!res.isSelect()) {
+    QMessageBox::warning(this, "", "Тип запроса не SELECT");
+    return;
+  }
+  ////
+  int rowNo = 0;
+  while (res.next()) {
+    rowNo++;
+    // --
+    auto row = res.record();
+    QString rowStr;
+    for (int i = 0; i < row.count(); ++i) {
+      auto fld = row.field(i);
+      QString fldName = row.fieldName(i);
+      rowStr += QString::asprintf(
+                 "[%s]: %s\n",
+                 fldName.toStdString().c_str(),
+                 fld.value().toString().toStdString().c_str());
+    }
+    // --
+    QMessageBox::information(this, QString::asprintf("СТРОКА: %d", rowNo), rowStr);
+  }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include "ui_mainwindow.h"
+#include <QtSql>
 
 class MainWindow : public QMainWindow, private Ui::MainWindow
 {
@@ -9,6 +10,11 @@ class MainWindow : public QMainWindow, private Ui::MainWindow
 
 public:
   explicit MainWindow(QWidget *parent = 0);
+
+  QSqlDatabase m_db;
+
+private slots:
+  void on_btnExecSQL_clicked();
 };
 
 #endif // MAINWINDOW_H
